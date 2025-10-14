5d327bb85ab7dd5fd7830fdbe7de0aa8120fc32b
Mate-Linux <mateoalvarez2012@gmail.com>
2025-10-13 21:03:13 -0300

    generar compose

--- workers/joiners/common/joiner.py
+++ workers/joiners/common/joiner.py
@@ -1,5 +1,5 @@
 from utils.file_utils.table_type import TableType, ResultTableType
-from utils.file_utils.process_table import TransactionItemsProcessRow, PurchasesPerUserStoreRow
+from utils.file_utils.process_table import TransactionItemsProcessRow, PurchasesPerUserStoreRow, TPVProcessRow
 from utils.file_utils.result_table import Query2_1ResultRow, Query2_2ResultRow, Query3ResultRow, Query4ResultRow
 from utils.file_utils.process_table import TableProcessRow
 from utils.file_utils.process_chunk import ProcessChunk
@@ -370,7 +370,7 @@ class MenuItemsJoiner(Joiner):
     
 class StoresTpvJoiner(Joiner):
     
-    def define_receivers(self):
+    def define_queues(self):
         self.data_receiver = MessageMiddlewareQueue("rabbitmq", "to_join_with_stores_tvp")
         self.data_join_receiver = MessageMiddlewareQueue("rabbitmq", "to_join_stores")
         self.data_sender = MessageMiddlewareQueue("rabbitmq", "to_merge_data")
@@ -379,12 +379,23 @@ class StoresTpvJoiner(Joiner):
         self.joiner_data[client_id][row.store_id] = row.store_name
     
     def join_result(self, row: TableProcessRow, client_id):
-        result = {
-            "store_id": row.store_id,
-            "store_name": self.joiner_data[client_id].get(row.store_id, "UNKNOWN"),
-            "tpv": row.final_amount,
-            "year_half": row.year_half_created_at,
-        }
+        # Soporte para TPVProcessRow
+        if hasattr(row, 'tpv') and hasattr(row, 'year_half'):
+            # TPVProcessRow
+            result = {
+                "store_id": row.store_id,
+                "store_name": self.joiner_data[client_id].get(row.store_id, "UNKNOWN"),
+                "tpv": row.tpv,
+                "year_half": row.year_half,
+            }
+        else:
+            # Fallback para TransactionsProcessRow (compatibilidad)
+            result = {
+                "store_id": row.store_id,
+                "store_name": self.joiner_data[client_id].get(row.store_id, "UNKNOWN"),
+                "tpv": row.final_amount,
+                "year_half": row.year_half_created_at,
+            }
         return result
     
     def publish_results(self, client_id):
@@ -405,6 +416,13 @@ class StoresTpvJoiner(Joiner):
             logging.info(f"action: sent_result_message | type:{self.joiner_type}")
         else:
             logging.info(f"action: no_results_to_send | type:{self.joiner_type} | client_id:{client_id}")
+    
+    def send_end_query_msg(self, client_id):
+        end_query_msg_3 = MessageQueryEnd(client_id, ResultTableType.QUERY_3, 1)
+        client_queue = MessageMiddlewareQueue("rabbitmq", f"to_merge_data_{client_id}")
+        client_queue.send(end_query_msg_3.encode())
+        client_queue.close()
+        logging.info(f"action: sent_end_query_message | type:{self.joiner_type} | client_id:{client_id}")
             
 class StoresTop3Joiner(Joiner):
     
