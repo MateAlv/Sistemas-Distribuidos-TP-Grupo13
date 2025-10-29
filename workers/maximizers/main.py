3e6d54020b4b3250a4dc473aee58276dc17bc252
Mate-Linux <mateoalvarez2012@gmail.com>
2025-10-29 18:52:49 -0300

    imports fix

--- workers/maximizers/main.py
+++ workers/maximizers/main.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import signal
 import logging
 import argparse
+from pathlib import Path
 from configparser import ConfigParser
+
+ROOT_DIR = Path(__file__).resolve().parents[2]
+if str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
 
 # Importar Maximizer
 from common import Maximizer
